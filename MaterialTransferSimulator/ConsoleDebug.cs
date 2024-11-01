7f1729a1c26b310723e78fbe60b39d73053c20f6
times <times@DESKTOP-0F99BL2>
2024-11-01 13:08:12 +0000

    Add project files.

---
+++ MaterialTransferSimulator/ConsoleDebug.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaterialTransferSimulator
+{
+    internal class ConsoleDebug
+    {
+        public static void Main()
+        {
+            Console.WriteLine("ok");
+            Container c = new Container()
+            {
+                name = "spoil",
+                currentVolume = 0,
+                capacity = 2500
+            };
+
+            Console.WriteLine(c.ToString());
+
+            Container d = new Container()
+            {
+                name = "excav",
+                currentVolume = 1000,
+                capacity = 1000
+            };
+
+            Console.WriteLine(d.ToString());
+
+            Transfer t = new Transfer()
+            {
+                linkFrom = d,
+                linkTo = c,
+                load = 50
+            };
+
+            t.SetName();
+            Console.WriteLine(t.ToString());
+
+            Simulation sim = new Simulation()
+            {
+                dateStart = new DateTime(2024, 10, 1),
+                dateEnd = new DateTime(2024,10,10)
+            };
+
+            Result res = sim.Run();
+
+
+        }
+    }
+}
---
+++ MaterialTransferSimulator/CoreClass.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaterialTransferSimulator
+{
+    public class Simulation
+    {
+        public string name = string.Empty;
+
+        public DateTime dateStart = DateTime.MinValue;
+        public DateTime dateEnd = DateTime.MinValue;  
+        public TimeSpan stepTime = TimeSpan.FromDays(1);
+
+        public List<Container> containers = new List<Container>();
+        public List<Transfer> transfers = new List<Transfer>();
+
+        public Result Run()
+        {
+            // initialise result and log
+            Result result = new Result();
+            result.entries = new List<LogEntry>();
+
+            LogEntry logEntry = new LogEntry();
+            logEntry.volumes = new List<double>();
+
+            DateTime currentDate = dateStart;
+
+            // cycle through simulation period
+            while (currentDate <= dateEnd)
+            {
+                // clear the log and set date
+                logEntry.Clear();
+                logEntry.SetDate(currentDate);
+
+                // collect current state of containers
+                foreach (Container c in containers)
+                {
+                    logEntry.Add(c.currentVolume);
+                }
+
+                Console.WriteLine(logEntry.ToString());
+
+                // update the result
+                result.Add(logEntry);
+
+                // transfer materials
+                foreach (Transfer t in transfers)
+                {
+                    // check transfer is valid
+                    // (is active at current date and has links From/To)
+                    if (t.dateStart <= currentDate & t.linkFrom != null & t.linkTo != null)
+                    {
+                        t.linkFrom.currentVolume -= t.load;
+                        t.linkTo.currentVolume -= t.load;
+                    }
+                }
+
+                currentDate += stepTime;
+
+            }
+
+            return result;
+        }
+    }
+
+    public class Container
+    {
+        public static int nextContainerId = 0;
+
+        public int id;
+        public string name = string.Empty;
+        public double currentVolume = 0;
+        public double capacity = 1000;
+        public double bulkOnImport = 1;
+        public double bulkOnExport = 1;
+
+        public Container() // default constructor
+        {
+            id = nextContainerId++;
+        }
+
+        public override string ToString()
+        {
+            return $"{id}:{name} - {currentVolume}";
+        }
+    }
+
+    public class Transfer
+    {
+        public static int nextTransferId = 0;
+
+        public int id;
+        public string name = string.Empty;
+        public double load = 0;
+        public Container linkFrom = null;
+        public Container linkTo = null;
+        public LoadBias loadBias = LoadBias.Transfer;
+        public DateTime dateStart = DateTime.MinValue;
+
+        public Transfer() // default constructor
+        {
+            id = nextTransferId++;
+        }
+
+        public void SetName()
+        {
+            string nameFrom = (linkFrom == null) ? "UnDef" : linkFrom.name;
+            string nameTo = (linkTo == null) ? "UnDef" : linkTo.name;
+            name = $"{nameFrom}-->{nameTo}";
+        }
+
+        public override string ToString()
+        {
+            return $"{id}:{name} - {load}";
+        }
+    }
+
+    public struct LogEntry
+    {
+        public DateTime date;
+        public List<double> volumes;
+
+        public void Clear()
+        {
+            date = DateTime.MinValue;
+            volumes.Clear();
+        }
+
+        public void SetDate(DateTime d)
+        {
+            date = d;
+        }
+
+        public void Add(double v)
+        {
+            volumes.Add(v);
+        }
+
+        public override string ToString()
+        {
+            string output = date.ToString("yyyy-MM-dd");
+            foreach (double v in volumes)
+            {
+                output += "\t0" + v.ToString();
+            }
+
+            return output;
+        }
+    }
+
+    public struct Result
+    {
+        public List<LogEntry> entries;
+
+        public void Add(LogEntry l)
+        {
+            entries.Add(l);
+        }
+    }
+
+    public enum LoadBias
+    {
+        Source = 0,
+        Transfer = 1,
+        Target = 2
+    }
+}
